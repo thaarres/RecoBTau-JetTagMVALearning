07be571a13957b3f2d43116d41328f7e9f6af6b4
pvmulder <pvmulder@cern.ch>
2014-04-08 22:04:31 +0200

    added trackJetPt to Save*xml and Train*xml files for CSV_MLP and CSVSL

--- test/CSV_MLP/VariableExtraction/merge_from_storage.sh
+++ test/CSV_MLP/VariableExtraction/merge_from_storage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-maindirec=/pnfs/iihe/cms/store/user/`whoami`/BtagExtractor_53X/NewVarsNewCat_modified1_tightTrackSel/
+maindirec=/pnfs/iihe/cms/store/user/`whoami`/BtagExtractor_5314_JetFlavourPUid/CSVV2_IVF/
 
-dirs_to_merge=(QCD_Pt-1000to1400_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM QCD_Pt-1400to1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM QCD_Pt-15to30_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-300to470_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM QCD_Pt-30to50_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-470to600_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-50to80_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-600to800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-800to1000_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-80to120_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM)
+dirs_to_merge=(QCD_Pt-1000to1400_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM QCD_Pt-120to170_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM QCD_Pt-1400to1800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM  QCD_Pt-170to300_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-300to470_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM  QCD_Pt-470to600_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-50to80_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-600to800_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-800to1000_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v2_AODSIM QCD_Pt-80to120_TuneZ2star_8TeV_pythia6_Summer12_DR53X-PU_S10_START53_V7A-v3_AODSIM)
 
 #dirs_to_merge=( TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1_AODSIM )
 
-homedirec=/user/pvmulder/NewEraOfDataAnalysis/BTagServiceWork/DEVELOPMENT/CMSSW_5_3_4_patch1/src/CrabJobs_modified1_tightTrackSel/RootFiles
+homedirec=/user/pvmulder/NewEraOfDataAnalysis/BTagServiceWork/DEVELOPMENT/RootFiles_CSVIVFV2_JetFlavourPUjetIDcleanIVF
 
 CAT=(CombinedSVV2RecoVertex CombinedSVV2PseudoVertex CombinedSVV2NoVertex)
 FLAV=(B C DUSG)
@@ -26,28 +26,28 @@ do
 			for j in $( ls $maindirec/$l/${k}_D*); do
 				echo "at file number $countfiles" 
 				((countfiles++))
-				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 20 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
+				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 5 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
 				printf "dcap://maite.iihe.ac.be/$j " >> ${k}DUSGfiles.txt
 			done
 			let countfiles=0
 			for j in $( ls $maindirec/$l/${k}_U*); do 
 				echo "at file number $countfiles" 
 				((countfiles++))
-				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 20 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
+				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 5 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
 				printf "dcap://maite.iihe.ac.be/$j " >> ${k}DUSGfiles.txt
 			done
 			let countfiles=0
 			for j in $( ls $maindirec/$l/${k}_S*); do 
 				echo "at file number $countfiles" 
 				((countfiles++))
-				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 20 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
+				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 5 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
 				printf "dcap://maite.iihe.ac.be/$j " >> ${k}DUSGfiles.txt
 			done
 			let countfiles=0
 			for j in $( ls $maindirec/$l/${k}_G*); do 
 				echo "at file number $countfiles" 
 				((countfiles++))
-				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 20 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
+				if [ $countfiles -ne 1 ] ; then if [ $countfiles -eq 5 ] ; then countfiles=0 ; fi ; continue ; fi #this makes sure that only 1/12 files is used!
 				printf "dcap://maite.iihe.ac.be/$j " >> ${k}DUSGfiles.txt
 			done
 		else
