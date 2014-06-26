c6c6ac7ecfc08328a499ae728d036af72d69244a
Thea Aarrestad <thea.aarrestad@cern.ch>
2014-06-26 17:38:07 +0200

    created mvaToSqlite and validation for my new CSVMLPIVF

---
+++ test/doubleB_CSVMLPIVF/CompareAllTaggers_copyMVAToSQLite_cfg.py
@@ -0,0 +1,67 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MVAJetTagsSQLiteSave")
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+
+#TO COMPARE ALL TAGGERS, USE THIS
+process.calib = cms.ESSource("BTauGenericMVAJetTagComputerFileSource",
+	CombinedSVRecoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVRecoVertex.mva'),
+	CombinedSVPseudoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVPseudoVertex.mva'),
+	CombinedSVNoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVNoVertex.mva'),
+	CombinedSVIVFV2RecoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVIVFV2RecoVertex.mva'),
+	CombinedSVIVFV2PseudoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVIVFV2PseudoVertex.mva'),
+	CombinedSVIVFV2NoVertex = cms.string('$HOME/CSVtraining/CMSSW_5_3_14/src/RecoBTau/JetTagMVALearning/test/CalibrationRecords/CombinedSVIVFV2NoVertex.mva'),
+	NEWCombinedSVIVFV2RecoVertex = cms.string('tmpRecoVertex/CombinedSVV2MVA_RecoVertex.mva'),
+	NEWCombinedSVIVFV2PseudoVertex = cms.string('tmpPseudoVertex/CombinedSVV2MVA_PseudoVertex.mva'),
+	NEWCombinedSVIVFV2NoVertex = cms.string('tmpNoVertex/CombinedSVV2MVA_NoVertex.mva'),
+)
+
+##TO ONLY LOOK AT CSVIVFV2, USE THIS
+#process.calib = cms.ESSource("BTauGenericMVAJetTagComputerFileSource",
+	#CombinedSVIVFV2RecoVertex = cms.string('CSV_MLP/tmpRecoVertex/CombinedSVV2MVA_RecoVertex.mva'), 
+	#CombinedSVIVFV2PseudoVertex = cms.string('CSV_MLP/tmpPseudoVertex/CombinedSVV2MVA_PseudoVertex.mva'), 
+	#CombinedSVIVFV2NoVertex = cms.string('CSV_MLP/tmpNoVertex/CombinedSVV2MVA_NoVertex.mva'), 
+#)
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+	BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+	DBParameters = cms.PSet( messageLevel = cms.untracked.int32(0) ),
+	timetype = cms.untracked.string('runnumber'),
+	connect = cms.string('sqlite_file:Validation/CompareAllTaggers_MVAJetTags.db'),
+	toPut = cms.VPSet(cms.PSet(
+		record = cms.string('BTauGenericMVAJetTagComputerRcd'),
+		tag = cms.string('MVAJetTags')
+	))
+)
+
+#TO COMPARE ALL TAGGERS, USE THIS
+process.jetTagMVATrainerSave = cms.EDAnalyzer("JetTagMVATrainerSave",
+     toPut = cms.vstring(),
+     toCopy = cms.vstring(
+	      'CombinedSVRecoVertex',
+	      'CombinedSVPseudoVertex',
+	      'CombinedSVNoVertex',
+	      'CombinedSVIVFV2RecoVertex',
+	      'CombinedSVIVFV2PseudoVertex',
+	      'CombinedSVIVFV2NoVertex',
+	      'NEWCombinedSVIVFV2RecoVertex',
+	      'NEWCombinedSVIVFV2PseudoVertex',
+	      'NEWCombinedSVIVFV2NoVertex',
+     )
+)
+     
+##TO ONLY LOOK AT CSVIVFV2, USE THIS
+#process.jetTagMVATrainerSave = cms.EDAnalyzer("JetTagMVATrainerSave",
+	#toPut = cms.vstring(),
+	#toCopy = cms.vstring(
+		#'CombinedSVIVFV2RecoVertex', 
+		#'CombinedSVIVFV2PseudoVertex', 
+		#'CombinedSVIVFV2NoVertex', 
+	#)
+#)
+
+process.outpath = cms.EndPath(process.jetTagMVATrainerSave)
